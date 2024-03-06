4e31494541eca7e97b6a6cf39e5cec0613e4b755
=oaksas <=>
2024-03-06 15:14:02 +0300

    Add models, serializers and mixin based generic view for catalogue

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
-from catalogue.serializers import UserSerializer
+from catalogue.serializers import UserSerializer, ArtistSerializer
+from rest_framework.generics import GenericAPIView
+from catalogue.models import Artist
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,3 +13,11 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
+
+
+class ArtistGenericView(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
