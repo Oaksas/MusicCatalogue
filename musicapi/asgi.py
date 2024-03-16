2dbd7d83884caf6961cddbb6b784373fc4c686a2
oaksas <aman.getnet2@gmail.com>
2024-03-16 18:24:03 +0300

    Fix consumer

--- musicapi/asgi.py
+++ musicapi/asgi.py
@@ -9,11 +9,18 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    'websocket':AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
 })
